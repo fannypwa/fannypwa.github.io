01a1e718a368ede07c9ac949e0082961d9980f8f
Fanny <fannycastilll@gmail.com>
2020-11-22 16:45:34 -0600

    Modificaion en funcionalidad.js y en index.html

---
+++ funcionalidad.js
@@ -0,0 +1,32 @@
+var operandoa;
+var operandob;
+var operacion;
+
+function init(){
+    //variables
+    var resultado = document.getElementById('resultado');
+  }
+  densidad.onclick = function(e){
+    operandoa = resultado.textContent;
+    operacion = "/";
+    limpiar();
+}
+function limpiar(){
+    resultado.textContent = "";
+  }
+  function resetear(){
+    resultado.textContent = "";
+    operandoa = 0;
+    operandob = 0;
+    operacion = "";
+  }
+  function resolver(){
+    var res = 0;
+    switch(operacion){
+      case "Calcular la Densidad":
+        res = parseFloat(operandoa) / parseFloat(operandob);
+        break;
+    }
+    resetear();
+    resultado.textContent = res;
+  }
