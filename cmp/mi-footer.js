31900a520fe18729be34543c97e1e2ca72bc2003
Fanny <fannycastilll@gmail.com>
2020-11-22 20:18:29 -0600

    Modificacion en index.html y mi-footer.js

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -1,7 +1,7 @@
 class MiFooter extends HTMLElement {
   connectedCallback() {
     this.innerHTML = /* html */
-      `Copyright &copy; Gilberto Pacheco Gallegos`;
+      `Copyright &copy; Castillo Castillo Estefania`;
   }
 }
 customElements.define("mi-footer", MiFooter);
